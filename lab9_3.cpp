7a383956b4f359fbce17a2ce8b235c0fb25ec1c9
fordsaranpong <forddbkzero@gmail.com>
2021-01-08 18:53:34 +0700

    lab9.3

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,25 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<cstdlib>
+#include<cmath>
+using namespace std;
+
+int main(){
+	float Mean,SD,Num,RN,S;
+	ifstream source;
+	source.open("C:\\Users\\Lelouch\\Desktop\\lab9\\score.txt");
+	string textline;
+	while(getline(source,textline)){
+	Mean+=atof(textline.c_str());
+	RN=RN+pow(atof(textline.c_str()),2);
+	Num++;}
+	source.close();
+    Mean=Mean/Num;
+    S=(RN/Num)-(Mean*Mean);
+	SD=pow(S,0.5);
+	cout << "Number of data = "<<Num;
+    cout << setprecision(3);
+    cout << "\nMean = "<<Mean;
+    cout << "\nStandard deviation = "<<SD;
+}
